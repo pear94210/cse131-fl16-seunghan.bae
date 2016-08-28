c40fbf0ccddb236a610b14953770126addbdd5ca
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2016-08-28 14:25:27 -0500

    How many pennies do you have?

--- exercises/exercises1/Change.java
+++ exercises/exercises1/Change.java
@@ -8,14 +8,27 @@ public class Change {
 		//
 		// Below, prompt the user to enter a number of pennies
 		//
-		
-		
+		ArgsProcessor money = new ArgsProcessor(args);		
+		int pennies = money.nextInt("How many pennies?");
 		//
 		// Then, compute and print out how many 
 		//    dollars, quarters, dimes, nickels, and pennies
 		// should be given in exchange for those pennies, so as to
 		// minimize the number of coins (see the videos)
 		//
+		int dollars = pennies / 100;
+		int leftPennies = pennies % 100;
+		
+		int quarters = leftPennies / 25;
+		leftPennies = leftPennies % 25;
+		
+		int dimes = leftPennies / 10;
+		leftPennies = leftPennies % 10;
+				
+		int nickels = leftPennies / 5;
+		leftPennies = leftPennies % 5;
+		
+		System.out.println("For "+pennies+" pennies: "+dollars+" dollars "+quarters+" quarters "+dimes+" dimes "+nickels+" nickels "+leftPennies+ " pennies");
 	}
 
 }
