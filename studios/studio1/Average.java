1bb33fb4d144f27fdff7dc4353ca80ffe065deca
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2016-08-30 13:47:53 -0500

    Some exercise for studio 1

---
+++ studios/studio1/Average.java
@@ -0,0 +1,19 @@
+package studio1;
+
+import cse131.ArgsProcessor;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArgsProcessor ap = new ArgsProcessor(args);
+		
+		int n1 = ap.nextInt("First integer?");
+		int n2 = ap.nextInt("Second integer?");
+		
+		double avg = (n1 + n2) / 2d;
+		
+		System.out.println("Average of " + n1 + " and " + n2 + " is " + avg + ".");
+	}
+
+}
