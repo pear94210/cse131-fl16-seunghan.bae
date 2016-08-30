2a5c2d5a55d6ed6077fc640133c2784df57520b2
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2016-08-30 13:29:45 -0500

    Nutrition 1

---
+++ labs/lab1/Nutrition.java
@@ -0,0 +1,13 @@
+package lab1;
+
+import cse131.ArgsProcessor;
+
+public class Nutrition {
+
+	public static void main(String[] args) {
+		
+		ArgsProcessor ap = new ArgsProcessor(args);
+		
+		
+	}
+}
