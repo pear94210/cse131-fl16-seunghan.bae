e399dce2bb8f3de8b5b795eeb541352028ac53d4
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2016-09-06 01:13:59 -0500

    Extension 2.1

--- extensions/imagetransforms/Transforms.java
+++ extensions/imagetransforms/Transforms.java
@@ -33,21 +33,50 @@ public class Transforms {
 	}
 	
 	public static void flipVert(Picture source, Picture target) {
-		// FIXME
+		for (int x = 0; x < source.width(); x++) {
+			for (int y = 0; y < source.height(); y++) {
+				int otherY = source.height() - 1 - y;
+				Color c = source.get(x, otherY);
+				target.set(x, y, c);
+			}
+		}
 	}
 	
 	
 	public static void flipHorizLeftHalf(Picture source, Picture target) {
-		// FIXME
+		for (int x = 0; x < source.width(); x++) {
+			for (int y = 0; y < source.height(); y++) {
+				if (x > (source.width() / 2)) {
+					int otherX = source.width() - 1 - x;
+					Color c = source.get(otherX, y);
+					target.set(x, y, c);
+				}
+				else target.set(x, y, source.get(x, y));
+			}
+		}
 	}
 	
 	public static void flipVertBotHalf(Picture source, Picture target) {
-		// FIXME
+		for (int x = 0; x < source.width(); x++) {
+			for (int y = 0; y < source.height(); y++) {
+				if (y > (source.height() / 2)) {
+					int otherY = source.height() - 1 - y;
+					Color c = source.get(x, otherY);
+					target.set(x, y, c);
+				}
+				else target.set(x, y, source.get(x, y));
+			}
+		}
 	}
 	
 	public static void gradient(Picture target) {
-		// FIXME
-				
+		for (int x = 0; x < target.width(); x++) {
+			for (int y = 0; y < target.height(); y++) {
+				int amountRed = (int) (255.0 * x / target.width());
+				int amountGreen = (int) (255.0 * y/ target.height());
+				target.set(x, y, new Color(amountRed, amountGreen, 128));
+			}
+		}		
 	}
 	
 	public static void edgeDetect(Picture source, Picture target) {
