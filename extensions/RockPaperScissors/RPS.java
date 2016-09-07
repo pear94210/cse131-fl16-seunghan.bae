4d213339ad82f53b1861237923839c7c0555963e
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2016-09-07 01:47:21 -0500

    Rock Paper Scissors v2

--- extensions/RockPaperScissors/RPS.java
+++ extensions/RockPaperScissors/RPS.java
@@ -12,6 +12,9 @@ public class RPS {
 		
 		int rotateNum = 0;
 		
+		int rotateWin = 0;
+		int randWin = 0;
+		
 		for (int i = 0; i < round; i++) {
 			int randNum = (int) (Math.random() * 3);
 			
@@ -36,7 +39,25 @@ public class RPS {
 					System.out.println("rock");
 				}
 			}
+			
+			if ((rotateNum - randNum == 1) || (randNum - rotateNum == 2)) {
+				System.out.println("\t" + "Rotating player wins");
+				rotateWin++;
+			}
+			else if ((randNum - rotateNum == 1) || (rotateNum - randNum == 2)) {
+				System.out.println("\t" + "Random player wins");
+				randWin++;
+			}
+			else System.out.println("\t" + "Draw");
 		}
+		
+		double rotatePercent = Math.round(1000.0 * rotateWin / round) / 10d;
+		double randPercent = Math.round(1000.0 * randWin / round) / 10d;
+		
+		System.out.println();
+		System.out.println("Random player won " + randPercent + "%");
+		System.out.println("Rotating player won " + rotatePercent + "%");
+		
 	}
 
 }
