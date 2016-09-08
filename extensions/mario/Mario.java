48aa00df4e50a2918c9b105c9388c14b9eb614db
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2016-09-08 14:28:59 -0500

    Extension 2 prep

---
+++ extensions/mario/Mario.java
@@ -0,0 +1,13 @@
+package mario;
+
+public class Mario {
+
+	public static void main(String[] args) {
+		
+		double begAmount = Math.round(4000.00 * 100) / 100;
+		double interest = 0.02;
+		
+		System.out.println(begAmount);
+	}
+
+}
