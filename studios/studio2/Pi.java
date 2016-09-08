a12d727e569e8bdebeebe8f1ee480e0d53b7de32
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2016-09-08 14:20:09 -0500

    Studio 2

--- studios/studio2/Pi.java
+++ studios/studio2/Pi.java
@@ -4,14 +4,32 @@ public class Pi {
 
 	public static void computePi() {
 		
-		double ans = 0.0;
 		
+		
+		double ans = 0.0;
+		int totalCount = 0;
+		int circleCount = 0;
+		double ratio = 0.0;
+		
+		for (int i = 0; i < 1000000; i++) {
+			totalCount++;
+			
+			double x = Math.random();
+			double y = Math.random();
+						
+			double length = Math.sqrt((x - 0.5) * (x - 0.5) + (y - 0.5) * (y - 0.5));
+			
+			if (length < 0.5) circleCount++;
+		}
+		
+		ratio = 1.0 * circleCount / totalCount;
+		ans = ratio / 0.25;
 		
 		System.out.println("Our group shows Pi = " + ans);
 
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		computePi();
 	}
 
--- studios/studio2/Ruin.java
+++ studios/studio2/Ruin.java
@@ -13,13 +13,42 @@ public class Ruin {
 		double lossChance = 1 - winChance;
 		int winAmount = ap.nextInt("Amount of money to make you win:");
 		int totalPlays = ap.nextInt("Number of simulations:");
+		int winCount = 0;
 		
-		//double expectedRuin;
+		for (int i = 0; i < totalPlays; i++) {
+			
+			int round = 0;
+			String winLose = "";
+			int roundAmount = startAmount;
+			
+			while (roundAmount > 0 && roundAmount < winAmount) {
+				round++;
+				
+				double random = Math.random();
+				
+				if (random < winChance) roundAmount++;
+				else roundAmount--;
+			}
+			
+			if (roundAmount == 0) winLose = "LOSE";
+			else if (roundAmount == winAmount) {
+				winLose = "WIN";
+				winCount++;
+			}
+			
+			System.out.println("Simulation " + (i + 1) + ": " + round + " rounds" + "     " + winLose);
+		}
 		
-		//if (winChance == lossChance) expectedRuin = 1 - (1.0 * startAmount / winAmount);
-		//else expectedRuin = ((lossChance / winChance))
+		double actualRuin = 1.0 * (totalPlays - winCount) / totalPlays;
+		double expectedRuin;
 		
-		//System.out.println("Expected Ruin Rate: " + expectedRuin);
+		if (winChance == lossChance) expectedRuin = 1 - (1.0 * startAmount / winAmount);
+		else expectedRuin = (Math.pow(lossChance / winChance, startAmount) - Math.pow(lossChance / winChance, winAmount)) / (1 - Math.pow(lossChance / winChance, winAmount));
+		
+		System.out.println();
+		System.out.println("Losses : " + (totalPlays - winCount) + "\t" + "Simulations: " + totalPlays);
+		System.out.println("Actual Ruin Rate: " + actualRuin);
+		System.out.println("Expected Ruin Rate: " + expectedRuin);
 	}
 
 }
