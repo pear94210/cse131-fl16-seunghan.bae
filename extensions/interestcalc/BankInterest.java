873676ec86d0a97b7db36835d7b426cfcb0859c0
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2016-09-08 15:25:38 -0500

    Extension 2.4 & 2.5

--- extensions/interestcalc/BankInterest.java
+++ extensions/interestcalc/BankInterest.java
@@ -3,8 +3,41 @@ package interestcalc;
 public class BankInterest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		double balance = 4000.0;
+		double interestRate = 0.02;
+		
+		int day = 0;
+		String type;
+		
+		System.out.println("Day" + "\t" + "Type    " + "\t" + "Amount" + "\t\t" + "Balance");
+		
+		for(int i = 0; i < 30; i++) {
+			day++;
+			double amount = 0.0;
+			
+			double random = Math.random();
+			
+			if(random < 0.5) {
+				amount = 100.0;
+				type = "Withdraw";
+				balance = balance - amount;
+			}
+			else {
+				amount = 200.5;
+				type = "Deposit ";
+				balance = balance + amount;
+			}
+			
+			System.out.println(day + "\t" + type + "\t" + "$" + amount + "0" + "\t\t" + "$" + balance + "0");
+		}
+		
+		double interest = balance * interestRate;
+		double total = balance + interest;
+		
+		System.out.println();
+		System.out.println("Interest earned: " + interest);
+		System.out.println("Money after one month\'s interest: " + total);
 	}
 
 }
--- extensions/mario/Mario.java
+++ extensions/mario/Mario.java
@@ -4,10 +4,36 @@ public class Mario {
 
 	public static void main(String[] args) {
 		
-		double begAmount = Math.round(4000.00 * 100) / 100;
-		double interest = 0.02;
+		for (int i = 0; i < 5; i++) {
+			for (int i1 = (4 - i); i1 > 0; i1--) System.out.print(" ");
+			for (int i2 = 0; i2 < (i + 1); i2++) System.out.print("#");
+			System.out.print("\n");
+		}
 		
-		System.out.println(begAmount);
+		System.out.println();
+		
+		for (int j = 0; j < 5; j++) {
+			for (int j1 = 0; j1 < (j + 1); j1++) System.out.print("#");
+			for (int j2 = (4 - j); j2 > 0; j2--) System.out.print(" ");
+			System.out.print("\n");
+		}
+		
+		System.out.println();
+
+		for (int k = 0; k < 5; k++) {
+			for (int k1 = 0; k1 < k; k1++) System.out.print(" ");
+			for (int k2 = (5 - k); k2 > 0; k2--) System.out.print("#");
+			System.out.print("\n");
+		}
+		
+		System.out.println();
+		
+		for (int l = 0; l < 5; l++) {
+			for (int l1 = (5 - l); l1 > 0; l1--) System.out.print("#");
+			for (int l2 = 0; l2 < l; l2++) System.out.print(" ");
+			System.out.print("\n");
+		}
+
 	}
 
 }
