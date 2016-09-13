ae5516bdbf587f7bc3168b6fd4c7cbd42334ccaa
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2016-09-13 14:00:26 -0500

    Exercises 3

--- exercises/exercises3/Shuffle.java
+++ exercises/exercises3/Shuffle.java
@@ -6,9 +6,7 @@ public class Shuffle {
 		String[] original = { "A", "B", "C", "D", "E", "F", "G", "H"};
 
 		// print out original array
-		for (int i=0; i < original.length; ++i) {
-			System.out.println("Original at " + i + " is " + original[i]);
-		}
+		for (int i = 0; i < original.length; i++) System.out.println("Original at " + i + " is " + original[i]);
 
 		//
 		// Follow the instructions on the web page to make a copy of
@@ -18,8 +16,19 @@ public class Shuffle {
 		// order.
 		//
 		
-		int shuffle = (int) (Math.random() * 8);
-
+		String[] shuffled = new String[original.length];
+		
+		for (int j = shuffled.length - 1; j >= 0; j--) {
+			int c = (int) (Math.random() * (j+1));
+			
+			shuffled[j] = original[c];
+			
+			for (int j2 = c; j2 < shuffled.length - 1; j2++) original[j2] = original[j2 + 1];
+		}
+		
+		System.out.println();
+		for (int k = 0; k < shuffled.length; k++) System.out.println("Shuffled at " + k + " is " + shuffled[k]);
+		
 	}
 	
 }
--- exercises/exercises3/TimesTable.java
+++ exercises/exercises3/TimesTable.java
@@ -9,8 +9,30 @@ public class TimesTable {
 		int N = ap.nextInt("Max value for table?");
 		
 		//  Table should include rows and columns for 0...N  
-		//     mkaing N+1 rows and columns
+		//     making N+1 rows and columns
 		
+		int[][] table = new int[N+1][N+1];
+		
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j <= N; j++) {
+				table[i][j] = i * j;
+			}
+		}
+		
+		System.out.print("   ");
+		for (int j = 0; j <= N; j++) System.out.print("   " + j);
+		System.out.print("\n");
+		System.out.println("-----------------------------------------------------------------");
+		
+		for (int i = 0; i <= N; i++) {
+			System.out.print(i + " | ");
+			
+			for (int j = 0; j <= N; j++) {
+				if (table[i][j] < 10) System.out.print("  " + table[i][j] + " ");
+				else System.out.print(" " + table[i][j] + " ");
+			}
+			System.out.print("\n");
+		}
 	}
 
 }
