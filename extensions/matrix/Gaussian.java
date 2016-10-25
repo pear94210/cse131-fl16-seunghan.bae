961beca56936b27240f2a9d35f0e2c006e7d7797
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2016-10-24 19:24:15 -0500

    Extension 7.2

--- extensions/matrix/Gaussian.java
+++ extensions/matrix/Gaussian.java
@@ -10,57 +10,56 @@ public class Gaussian {
 		this.constants = matConst;
 	}
 	
-	
 	/**
 	 * The hard part!
 	 * @return
 	 */
 	public Matrix getSolution(){
 		for (int i = 0; i < this.coeff.getNumCols(); i++) {
-			for (int j = i + 1; j < this.coeff.getNumRows(); j++) {
-				int k = i + 1;
-				while (this.coeff.getValue(i, i) != 0) {
-					this.coeff.exchangeRows(i, k);
-					this.constants.exchangeRows(i, k);
-					k++;
+			for (int n = i + 1; n < this.coeff.getNumRows(); n++) {
+				if (this.coeff.getValue(i, i) == 0) {
+					this.coeff.exchangeRows(i, n);
+					this.constants.exchangeRows(i, n);
 				}
-				
-				this.coeff.scaleRow(i, 1.0 / this.coeff.getValue(i, i));
-				this.constants.scaleRow(i,  1.0 / this.coeff.getValue(i, i));
-					
+			}
+			
+			this.coeff.scaleRow(i, 1.0 / this.coeff.getValue(i, i));
+			this.constants.scaleRow(i, 1.0 / this.coeff.getValue(i, i));
+			
+			for (int j = i + 1; j < this.coeff.getNumRows(); j++) {
 				if (this.coeff.getValue(j, i) != 0) {
 					this.coeff.scaleRow(j, -1.0 / this.coeff.getValue(j, i));
 					this.constants.scaleRow(j, -1.0 / this.coeff.getValue(j, i));
+					
 					this.coeff.addRows(i, j);
 					this.constants.addRows(i, j);
 				}
 			}
 		}
 		
+		for (int i = 0; i < this.coeff.getNumRows(); i++) {
+			this.coeff.scaleRow(i, 1.0 / this.coeff.getValue(i, i));
+			this.constants.scaleRow(i, 1.0 / this.coeff.getValue(i, i));
+		}
+		
 		for (int i = 1; i < this.coeff.getNumCols(); i++) {
-			for (int j = 0; j < i - 1; j++) {
-				if (this.coeff.getValue(i, i) != 0) {
-					this.coeff.scaleRow(i, 1.0 / this.coeff.getValue(i, i));
-					this.constants.scaleRow(i,  1.0 / this.coeff.getValue(i, i));
+			for (int j = 0; j < i; j++) {
+				if (this.coeff.getValue(j, i) != 0) {
+					this.coeff.scaleRow(j, -1.0 / this.coeff.getValue(j, i));
+					this.constants.scaleRow(j, -1.0 / this.coeff.getValue(j, i));
 					
-					if (this.coeff.getValue(j, i) != 0) {
-						this.coeff.scaleRow(j, -1.0 / this.coeff.getValue(j, i));
-						this.constants.scaleRow(j, -1.0 / this.coeff.getValue(j, i));
-						this.coeff.addRows(i, j);
-						this.constants.addRows(i, j);
-					}
+					this.coeff.addRows(i, j);
+					this.constants.addRows(i, j);
 				}
 			}
 		}
 		
 		for (int i = 0; i < this.coeff.getNumRows(); i++) {
-			if (this.coeff.getValue(i,  i) != 0) {
-				this.coeff.scaleRow(i, -1.0 / this.coeff.getValue(i, i));
-				this.constants.scaleRow(i, -1.0 / this.coeff.getValue(i, i));
-			}
+			this.coeff.scaleRow(i, 1.0 / this.coeff.getValue(i, i));
+			this.constants.scaleRow(i, 1.0 / this.coeff.getValue(i, i));
 		}
 		
-		return this.coeff;
+		return this.constants;
 	}
 	
 	public String toString(){
