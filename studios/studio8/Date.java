121ad7b1e8050b518c0474e7564b86e387979a57
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2016-10-26 00:04:50 -0500

    Studio 8 prep

--- studios/studio8/Date.java
+++ studios/studio8/Date.java
@@ -2,7 +2,16 @@ package studio8;
 
 public class Date {
 	
+	final private int month, day, year;
+	final private boolean holiday;
 	
+	public Date(int month, int day, int year, boolean holiday) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		this.holiday = holiday;
+	}
+
 	public static void main(String[] args) {
 	}
 
--- studios/studio8/Time.java
+++ studios/studio8/Time.java
@@ -2,7 +2,15 @@ package studio8;
 
 public class Time {
 	
-	
+	final private int hour, minute;
+	final private boolean twelveHour;
+		
+	public Time(int hour, int minute, boolean twelveHour) {
+		this.hour = hour;
+		this.minute = minute;
+		this.twelveHour = twelveHour;
+	}
+
 	public static void main(String[] args) {
 	}
 
