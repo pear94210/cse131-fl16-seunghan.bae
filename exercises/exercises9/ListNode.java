e5809873762571c1f3b3f7f362d31d4536d817f2
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2016-11-02 13:44:34 -0500

    Exercise 9

--- exercises/exercises9/ListNode.java
+++ exercises/exercises9/ListNode.java
@@ -14,11 +14,29 @@ public class ListNode<T> {
 	//
 	// static and iterative
 	//
-	public static<T> int size(ListNode<T> start) {
-		// NB  start could be null!
-		return 0; // FIXME
+	public static<T> int sizeIterative(ListNode<T> start) {
+		int size = 0;
+		for (ListNode<T> p = start; p != null; p = p.next) {
+			size++;
+		}
+		return size;
 	}
-
+	
+	//
+	// static and recursive
+	//
+	public static<T> int size(ListNode<T> start) {
+		if (start == null) return 0;
+		else return 1 + size(start.next);
+	}
+	
+	//
+	// static and recursive
+	//
+	public static<T> int sum(ListNode<T> start) {
+		if (start == null) return 0;
+		else return (int)start.value + sum(start.next);
+	}
 	
 	public static void main(String[] args) {
 		ListNode<Integer> head = new ListNode<Integer>(131);
@@ -29,16 +47,16 @@ public class ListNode<T> {
 				+ " " + head.next.value
 				+ " " + head.next.next.value 
 				+ " " + head.next.next.next.value);
-//		System.out.println("From start " + size(head));
-//		System.out.println(" next " + size(head.next));
-//		System.out.println(" then " + size(head.next.next));
-//		System.out.println(" then " + size(head.next.next.next));
-//		System.out.println(" end " + size(head.next.next.next.next));
-//		System.out.println("From start " + sum(head));
-//		System.out.println(" next " + sum(head.next));
-//		System.out.println(" then " + sum(head.next.next));
-//		System.out.println(" then " + sum(head.next.next.next));
-//		System.out.println(" end " + sum(head.next.next.next.next));
+		System.out.println("From start " + size(head));
+		System.out.println(" next " + size(head.next));
+		System.out.println(" then " + size(head.next.next));
+		System.out.println(" then " + size(head.next.next.next));
+		System.out.println(" end " + size(head.next.next.next.next));
+		System.out.println("From start " + sum(head));
+		System.out.println(" next " + sum(head.next));
+		System.out.println(" then " + sum(head.next.next));
+		System.out.println(" then " + sum(head.next.next.next));
+		System.out.println(" end " + sum(head.next.next.next.next));
 	}
 
 }
